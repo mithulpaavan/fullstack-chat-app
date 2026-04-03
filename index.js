81fabac5937279df2adbef10e4ca7d9751d75bb2
PATRICK <mithulpaavan01@gmail.com>
2026-04-03 20:03:49 +0530

    hello

--- index.js
+++ index.js
@@ -8,27 +8,11 @@ const newsRouter = require('./router/newsRouter.js');
 const cookieParser = require('cookie-parser');
 
 const mongoose = require('mongoose');
-
-const cors = require("cors");
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://timely-gaffire-dc6dcc.netlify.app"
-];
-
+const cors = require('cors');
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  origin: 'https://timely-gaufre-dc6dfc.netlify.app',
+  credentials: true
 }));
-app.options("/*", cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
